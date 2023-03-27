5cc768a4ca8b3b7739cab11b56110ec69b02206f
Georgii Bondarenko <bgv900@mail.ru>
2023-03-27 19:34:22 +0300

    ДЗ готово

--- Task_41/Program.cs
+++ Task_41/Program.cs
@@ -1,7 +1,7 @@
 ﻿#pragma warning disable
 Console.Clear();
 Console.WriteLine("Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.");
-Console.Write("Введите количество элементов массива:\t");
+Console.Write("\nВведите количество элементов массива:\t");
 
 int elementsCount;
 int.TryParse(Console.ReadLine()!, out elementsCount);
@@ -12,7 +12,9 @@ for (int i = 0; i < Array.Length; i++)
     Console.Write($"Введите элемент массива с индексом {i}:\t");
     Array[i] = int.Parse(Console.ReadLine());
 }
+
 int sum = 0;
+
 for (int i = 0; i < Array.Length; i++)
 {
     if (Array[i] > 0)
@@ -22,3 +24,18 @@ for (int i = 0; i < Array.Length; i++)
 }
 Console.WriteLine();
 Console.WriteLine($"количество значений больше 0 = {sum}");
+
+static int[] CreateArray(int elementsCount) 
+{ 
+    // содаем пустой массив из n элементов 
+    int[] result = new int[elementsCount]; 
+     
+    for (int i = 0; i < elementsCount; i++) 
+    { 
+        Console.Write($"Введите элемент массива с индексом {i}:\t"); 
+        result[i] = int.Parse(Console.ReadLine()); 
+    } 
+ 
+    // возвразщаем заполненный массив 
+    return result; 
+}
---
+++ Task_43/Program.cs
@@ -0,0 +1,21 @@
+﻿#pragma warning disable
+//Console.Clear();
+Console.WriteLine("Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.");
+
+Console.Write("\nВведите значение b1:\t");
+double b1;
+double.TryParse(Console.ReadLine()!, out b1);
+Console.Write("\nВведите значение k1:\t");
+double k1;
+double.TryParse(Console.ReadLine()!, out k1);
+Console.Write("\nВведите значение b2:\t");
+double b2;
+double.TryParse(Console.ReadLine()!, out b2);
+Console.Write("\nВведите значение k1:\t");
+double k2;
+double.TryParse(Console.ReadLine()!, out k2);
+
+double x = (b2 - b1)/(k1 - k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
